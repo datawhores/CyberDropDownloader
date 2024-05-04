aaffc13db82bc815772fa638478ed801e3edf893
johnsmih <you@example.com>
2024-05-04 16:16:56 -0500

    hash

--- cyberdrop_dl/utils/database/table_definitions.py
+++ cyberdrop_dl/utils/database/table_definitions.py
@@ -1,24 +1,26 @@
 create_history = """CREATE TABLE IF NOT EXISTS media (domain TEXT,
                                                url_path TEXT,
                                                referer TEXT,
-                                               album_id TEXT,
                                                download_path TEXT,
                                                download_filename TEXT,
                                                original_filename TEXT,
                                                completed INTEGER NOT NULL,
-                                               created_at TIMESTAMP,
-                                               completed_at TIMESTAMP,
+                                               hash text,
+                                               size INTEGER,
+                                               dupe INTEGER NOT NULL,
                                                PRIMARY KEY (domain, url_path, original_filename)
                                                );"""
 
 create_fixed_history = """CREATE TABLE IF NOT EXISTS media_copy (domain TEXT,
                                                url_path TEXT,
                                                referer TEXT,
-                                               album_id TEXT,
                                                download_path TEXT,
                                                download_filename TEXT,
                                                original_filename TEXT,
                                                completed INTEGER NOT NULL,
+                                               hash text,
+                                               size INTEGER,
+                                               dupe INTEGER NOT NULL,
                                                PRIMARY KEY (domain, url_path, original_filename)
                                                );"""
 
