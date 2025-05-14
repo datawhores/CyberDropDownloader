6e1a7497b993d3f2294d29033df52f7eab94f0e6
datawhores <datawhores@users.noreply.github.com>
2025-05-14 01:17:55 -0500

    remove ui

--- cyberdrop_dl/utils/utilities.py
+++ cyberdrop_dl/utils/utilities.py
@@ -94,7 +94,7 @@ def error_handling_wrapper(
             return await func(*args, **kwargs)
         except CDLBaseError as e:
             # error_log_msg = ErrorLogMessage(e.ui_failure, str(e))
-            error_log_msg = ("", str(e))
+            error_log_msg = ErrorLogMessage("", str(e))
             origin = e.origin
             e_url: URL | str | None = getattr(e, "url", None)
             link_to_show = e_url or link_to_show
