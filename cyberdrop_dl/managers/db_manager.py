08a3a0b2a2134e7b9d681589a9a1def99c2ea732
datawhores <datawhores@users.noreply.github.com>
2025-07-26 01:07:12 -0500

    add timeout

--- cyberdrop_dl/managers/db_manager.py
+++ cyberdrop_dl/managers/db_manager.py
@@ -29,7 +29,7 @@ class DBManager:
 
     async def startup(self) -> None:
         """Startup process for the DBManager."""
-        self._db_conn = await aiosqlite.connect(self._db_path)
+        self._db_conn = await aiosqlite.connect(self._db_path, timeout=120)
 
         self.ignore_history = self.manager.config_manager.settings_data.runtime_options.ignore_history
 
